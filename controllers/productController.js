c70856285547030870f77e1e780c3f9e8c371407
Kelli Libin <km.putnam2@gmail.com>
2023-06-09 15:06:59 -0600

    added functionality to product paths

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,14 +6,15 @@ const { StatusCodes } = require("http-status-codes");
 
 //controller
 const createProduct = async (req, res) => {
-  res.send("create product");
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({product})
 };
 const getAllProducts = async (req, res) => {
-  res.send("get all products");
+  const products = await Product.find({})
+  res.status(StatusCodes.OK).json({products})
 };
 
-
 module.exports = {
-    createProduct,
-    getAllProducts
-}
+  createProduct,
+  getAllProducts,
+};
