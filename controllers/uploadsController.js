9b2098c217a44f43dfab065e87d0d6e5c06147bb
Kelli Libin <km.putnam2@gmail.com>
2023-06-09 15:06:40 -0600

    added functionality for uploading images

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,17 @@
+const path = require('path');
 const {StatusCodes} = require('http-status-codes');
 
 //controller
 const uploadProductImage = async (req, res) => {
-    res.send('upload image');
+    //console log req.files, you can finally see the image, the name, the data, size, etc. use .mv (move) to store it on our server.
+    //then make it publically available.
+    let productImage = req.files.image;
+    //create a path to pass to the mv function. looking for the path whree the image needs to be moved, then name
+    const imagePath = path.join(__dirname, '../public/uploads/'+`${productImage.name}`);
+    await productImage.mv(imagePath);
+    //now send back correct path
+
+    return res.status(StatusCodes.OK).json({image: {src: `/uploads/${productImage.name}`}})
 };
 
 
